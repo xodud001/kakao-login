4fd186e02d4ae294484e34d7973c1b47444c1af9
김태영 <freeptk1@gmail.com>
2022-04-26 23:42:26 +0900

    프로젝트 초기화 및 카카오 로그인 테스트

---
+++ src/main/java/kakao/login/kakaologin/controller/LoginController.java
@@ -0,0 +1,13 @@
+package kakao.login.kakaologin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "loginForm";
+    }
+}
